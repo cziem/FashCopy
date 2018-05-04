155edd00db5882d0931f41b8a6a537d962afb3d8
Fay Vor <phavorsparks@gmail.com>
2018-05-04 17:04:02 +0100

    Initial commit

---
+++ ruf.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo Enter filename to copy:
+read -r files
+echo
+echo Enter Directory to copy files to:
+read -r dir
+echo
+echo copying file\(s\) $files to $dir directory
+
+#check if file exists
+for file in $files; do
+     if [ ! -e "$file" ]; then
+         echo file does not exist
+         exit 0
+     else
+        if [ ! -d "$dir" ]; then #check if dir exists
+            mkdir $dir
+            echo $dir
+        fi
+        cp "$file" "$dir"
+     fi
+ done
+ ls $dir
