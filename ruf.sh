61635ec63cef3984d612c3af965afd6dd773499a
Fay Vor <phavorsparks@gmail.com>
2018-05-04 17:19:23 +0100

    Update ruf.sh

--- ruf.sh
+++ ruf.sh
@@ -10,15 +10,16 @@ echo copying file\(s\) $files to $dir directory
 
 #check if file exists
 for file in $files; do
-     if [ ! -e "$file" ]; then
-         echo file does not exist
-         exit 0
-     else
+    if [ ! -e "$file" ]; then
+        echo file does not exist
+        exit 0
+    else
         if [ ! -d "$dir" ]; then #check if dir exists
             mkdir $dir
             echo $dir
         fi
         cp "$file" "$dir"
-     fi
- done
- ls $dir
+    fi
+done
+ls $dir
+echo 'Copying is done'
