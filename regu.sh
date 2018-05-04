8d3bb594dfd286224604c6eebcaeaee21baaae4c
Fay Vor <phavorsparks@gmail.com>
2018-05-04 17:15:17 +0100

    Update regu.sh

--- regu.sh
+++ regu.sh
@@ -1,24 +1,16 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# echo Enter name:
-# read name
-# echo 'Enter friend: '
-# read friend
+echo Enter name:
+read name
+echo 'Enter friend: '
+read friend
 
-# if [ $name ] && [ $friend ]; then
-#     echo passed
-#     echo
-#     echo Your name is $name and your friends name is $friend
-# elif [ ! $name ] && [ $friend ]; then
-#     echo failed
-#     echo
-#     echo parameters too short
-# fi
-
-
-read -p "Do you wish to install this program?" yn
-case $yn in
-    [Yy]* ) echo yes; break;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer yes or no.";;
-esac
+if [ $name ] && [ $friend ]; then
+    echo passed
+    echo
+    echo Your name is $name and your friends name is $friend
+elif [ ! $name ] && [ $friend ]; then
+    echo failed
+    echo
+    echo parameters too short
+fi
