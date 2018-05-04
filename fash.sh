634282fa3173a01918b6f5b8ee5d7875249d9d38
Fay Vor <phavorsparks@gmail.com>
2018-05-04 17:16:38 +0100

    rename dili.sh to fash.sh

--- fash.sh
+++ fash.sh
@@ -1,55 +1,38 @@
 #!/bin/bash
-Loop=3  # How many times to get input.
-ParamsCount=1
-message="Enter download link: "
-type="Download Link: "
-DownloadLink=""
-filename=""
-#Destination
 
-getUserParams () {
-  local local_var
-  echo -n $message
-  read local_var
-  [ -n "$local_var" ] && eval $1=\$local_var
-}
+echo 'Enter name of file to be copied: '
+read dFile
+echo 'Enter name of destination directory: '
+read dDir
+echo 'Checking exceptions'
 
-
-while [ "$ParamsCount" -le "$Loop" ]
-do
-  getUserParams var
-  echo "$var"
-if [ $ParamsCount -eq 1 ]
- then
-  DownloadLink="$var";
-  message="Enter Destination folder: "
-  type="Destination"
-else
-if [ "$type" == "Destination" ]
-  then 
-   Destination="$var"
-   message="Enter filename: "
-   type="filename"
-  else
-   filename="$var"
-  fi
+if [ ! -e "$dFile" ] && [ ! -d "$dDir" ]; then # File and folder do not exist
+    echo 'File path and name does not exist'
+    echo 'Destination does not exist'
+    exit 1
+elif [ ! -e "$dFile" ] && [ -d "$dDir" ]; then # File does not exist while folder does
+    echo 'Sorry! file does not exist.'
+    # ask user to agree to create the file
+    echo ============================
+    echo 'Would you like to create the file? Yes [y] or No [n]'
+    case $yn in
+        [Yy] ) touch $dFile;;
+        [Nn] ) exit;;
+        * )
+    esac
+    echo 'File created... copying to the designated folder'
+    cp $dFile $dDir
+    ls $dDir
+elif [ -e "$dFile" ] && [ ! -d "$dDir" ]; then # File exists and floder does not
+    echo 'The designated directory does not exist'
+    echo 'Creating directory'
+    mkdir $dDir
+    echo
+    echo 'Copying files now'
+    cp $dFile $dDir
+    ls $dDir
+else # File and folder exists
+    echo 'Copying files now'
+    cp $dFile $dDir
+    ls $dDir
 fi
-let "ParamsCount += 1"
-  echo
-done  
-echo "Download Link: $DownloadLink; filename : $filename; Destination : $Destination"
-echo
-echo "Stating Download"
-echo "Destination/$filename $DownloadLink"
-if [ ! -d "~$Destination" ]; then
-  mkdir ~$Destination
-  echo ~$Destination
-fi
-if ! which curl >/dev/null; then
-   sudo -k apt-get update
-   sudo -k apt-get install curl -y
-fi
-    curl -o ~$Destination/$filename $DownloadLink 
-    echo "Download Complete"
-echo
-exit 0
